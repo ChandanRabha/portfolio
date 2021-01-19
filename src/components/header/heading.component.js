9ef5b94e608f90e7c47ada725d6eab8aa9c24656
ChandanRabha <changrabha.rabha1@gmail.com>
2021-01-20 04:25:05 +0530

    make it better

--- src/components/header/heading.component.js
+++ src/components/header/heading.component.js
@@ -2,14 +2,24 @@ import './heading.component.css'
 import HeaderMenus from '../header-menus/header-menu.component.js'
 
  export const Heading = () => {
+   function myFunc(e){
+      var x = document.getElementById("menus");
+      if (x.className === "menus") {
+        x.className += " responsive";
+      } else {
+        x.className = "menus";
+  }
+   }
   return (
     <div className='header'>
        <h1 className='animate__animated animate__rollIn'>CR</h1> 
-       <ul className='menus'>
+       <ul className='menus' id="menus">
          <HeaderMenus title='Home' />
-         <HeaderMenus title='About Me' />
+         <HeaderMenus title='About' />
          <HeaderMenus title='Projects' />
-         <HeaderMenus title='Contact Me' />
+         <HeaderMenus title='Contact' />
+         <li className='icon' onClick={myFunc}>X</li>
        </ul>
+      
     </div>);
 }
