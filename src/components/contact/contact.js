25ee5ac81b077bf216bb38d00e10120eb84419ec
ChandanRabha <changrabha.rabha1@gmail.com>
2021-01-25 19:32:05 +0530

    Make it better

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,7 +2,7 @@ import './contact.css'
 export const Contact = () => {
     return (
         <div id="Contact">
-            <div style={{padding:'2%',backgroundColor:'rgb(240 239 224 / 90%)',boxShadow:'1px 3px 4px grey',minWidth:'35%',borderRadius:'5px',margin:'3%'}}>
+            <div>
             <h2>Contact Me</h2>
             <div><h3>Name:</h3><input type='text' required placeholder="Your Name?"/></div>
             <div><h3>Email:</h3><input type='email' required placeholder="Your Email?"/></div>
--- src/components/projects/individual_projects/individual_projects.component.js
+++ src/components/projects/individual_projects/individual_projects.component.js
@@ -6,6 +6,7 @@ export const IndividualProjects = (props) => {
         <div>
                <h1>{props.eachProject[0]}</h1>
                <img src={props.eachProject[1]} alt="Project_Image"></img>
+               <div id='ProjectLinks'><button> <i className="fab fa-github fa-2x"></i> View on Github</button> <button> <i className="fas fa-search fa-2x"></i> Visit Application</button></div>
         </div>
     )
 }
