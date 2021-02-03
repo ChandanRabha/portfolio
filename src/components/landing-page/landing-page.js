c5881186b7468b13c2766c114e6a59f261f78dfa
Chandan Rabha <changrabha.rabha1@gmail.com>
2021-02-03 15:46:42 +0530

    forgot change id in explore button

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -8,7 +8,7 @@ export const Landing = () => {
             <div style={{textAlign:'center',textShadow: '5px 1px grey'}}>
                  <h1 className='animate__animated animate__fadeInLeft'>Hi, My Name is Chandan Rabha</h1>
                  <h1 className='animate__animated animate__fadeIn animate__slow'>Welcome To My Dungeon</h1>
-                 <a className='animate__animated animate__fadeIn animate__delay-1s' href="#About" id='explore'>Explore</a>
+                 <a className='animate__animated animate__fadeIn animate__delay-1s' href="#Pin_About" id='explore'>Explore</a>
             </div>
             {/* <div style={{width:'300px',position:"relative"}}>
                 <div className="pentagon"></div>
